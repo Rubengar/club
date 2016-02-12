ed0c5934307233c7598d4a4c9d9bd818f5fc1569
unknown <you@example.com>
2016-02-12 09:31:16 +0100

    Añadido el metodo joinedMonth()

--- Club.java
+++ Club.java
@@ -9,7 +9,7 @@ public class Club
 {
     // Define any necessary fields here ...
     private ArrayList<Membership> socios;
-    
+
     /**
      * Constructor for objects of class Club
      */
@@ -35,5 +35,33 @@ public class Club
     public int numberOfMembers()
     {
         return socios.size();
+    }
+
+    /** 
+     * Calcula el numero de socios que se dieron de alta en un mes determinado. 
+     * El año no nos importa. En caso de que el parametro contenga un valor 
+     * no valido se muestra por pantalla el error.
+     * @param month El mes en el que estamos interesados
+     * @return El numero de socios que se dieron de alta dicho mes
+     */
+    public int joinedMonth(int month)
+    {
+        int numSocios = 0;
+        if(month > 12 || month < 1)
+        {
+            System.out.println("¡¡ERROR!! El mes es incorrecto");
+
+        }
+        else
+        {
+            for(Membership socio:socios)
+            {
+                if(month == socio.getMonth())
+                {
+                    numSocios++;
+                }
+            }
+        }
+        return numSocios++;
     }
 }
